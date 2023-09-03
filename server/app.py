c28a7fe9bf33f63b9dbb96574c6dd3c6d19d5288
Daniel Cornejo <danielcornejoaaa@gmail.com>
2023-09-03 18:33:35 -0400

    Finally

--- server/app.py
+++ server/app.py
@@ -21,24 +21,27 @@ def home():
 def animal_by_id(id):
     animal = Animal.query.filter(Animal.id == id).first()
     if animal:
-        return f"<ul><li>Name: {animal.name}</li><li>Species: {animal.species}</li><li>Zookeeper: {animal.zookeeper.name}</li><li>Enclosure: {animal.enclosure.environment}</li></ul>"
-    return make_response("Animal not found", 404)
+        return f"<ul>Name: <li>{animal.name}</li></ul><ul>Species: <li>{animal.species}</li></ul><ul>Zookeeper: <li>{animal.zookeeper.name}</li></ul><ul>Enclosure: <li>{animal.enclosure.environment}</li></ul>"
+    else:
+        return make_response("Animal not found", 404)
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
     zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
     if zookeeper:
-        animals = "<li>".join([animal.name for animal in zookeeper.animals])
-        return f"<ul><li>Name: {zookeeper.name}</li><li>Birthday: {zookeeper.birthday}</li><li>Animals: <ul><li>{animals}</li></ul></li></ul>"
-    return make_response("Zookeeper not found", 404)
+        animal_names = "<li>".join([animal.name for animal in zookeeper.animals])
+        return f"<ul>Name: <li>{zookeeper.name}</li></ul><ul>Birthday: <li>{zookeeper.birthday}</li></ul><ul>Animals: <li>{animal_names}</li></ul>"
+    else:
+        return make_response("Zookeeper not found", 404)
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
     enclosure = Enclosure.query.filter(Enclosure.id == id).first()
     if enclosure:
-        animals = "<li>".join([animal.name for animal in enclosure.animals])
-        return f"<ul><li>Environment: {enclosure.environment}</li><li>Open to Visitors: {enclosure.open_to_visitors}</li><li>Animals: <ul><li>{animals}</li></ul></li></ul>"
-    return make_response("Enclosure not found", 404)
+        animal_names = "<li>".join([animal.name for animal in enclosure.animals])
+        return f"<ul>Environment: <li>{enclosure.environment}</li></ul><ul>Open to Visitors: <li>{enclosure.open_to_visitors}</li></ul><ul>Animals: <li>{animal_names}</li></ul>"
+    else:
+        return make_response("Enclosure not found", 404)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,19 +1,19 @@
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from datetime import date
 
 db = SQLAlchemy()
 
 class Animal(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    species = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String(50), nullable=False, default='Unnamed')
+    species = db.Column(db.String(50), nullable=False, default='Unknown')
     zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeeper.id'), nullable=False)
     enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosure.id'), nullable=False)
 
 class Zookeeper(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    birthday = db.Column(db.Date, nullable=False)
+    name = db.Column(db.String(50), nullable=False, default='Unnamed')
+    birthday = db.Column(db.Date, nullable=False, default=date.today())
     animals = db.relationship('Animal', backref='zookeeper')
 
 class Enclosure(db.Model):
